29d924060083fa09aa4b402f180a0219e758f572
Lucas Gusmão <lucasgusmaobaptista@gmail.com>
2024-10-22 17:38:58 -0300

    Configuring addCorsMappings

---
+++ src/main/java/com/lucasgusmao/todosimple/Config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.lucasgusmao.todosimple.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
